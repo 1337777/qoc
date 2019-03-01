161e1e77a400626b192a7521f01c17c61744945c
user1@localhost <user1@localhost>
2019-03-02 01:21:59 +0800

    init

--- qoc_jisuanji.v
+++ qoc_jisuanji.v
@@ -14,3 +14,7 @@ From Qoc Require Import Jisuanji.
 结束 section1.
 
 Check ( 对于所有 (n m : nat) (b : bool), n = if b then n else m ). 
+
+Check (if true then 2 else 3).
+Compute (if true then 2 else 3).
+Compute ( 如果 true 然后 2 否则 3  ).
