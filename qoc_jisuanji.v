09948a144e35526f04f588c47780a8cdd17d51e7
user1@localhost <user1@localhost>
2019-03-08 22:47:12 +0800

    init

--- qoc_jisuanji.v
+++ qoc_jisuanji.v
@@ -1,12 +1,44 @@
 From Qoc Require Import Jisuanji.
 
+归纳义 我的二进制  := 真 : 我的二进制 | 假 : 我的二进制 .
+
+定义 或 ( x : 我的二进制 ) ( y : 我的二进制 ) := ( 若 x 则 真 否则 y ) . 
+
+部分 我的分1.
+  变量 x : 我的二进制.
+  变量 y : 我的二进制.
+
+  论点 我的论点1 : 或 x y  =  或 y x .
+  证明.
+    展开 或.
+    例子 x.
+    - 例子 y.
+      + 同一.
+      + 同一.
+    - 例子 y.
+      + 同一.
+      + 同一.
+   据证实.
+结束 我的分1.
+
+
+
+
+
+
+
+
+
+
+(** -------------------------------------------- **)
+
 参数 myparam : nat.
 
 部分 section1.
 
 变量 myvar : nat.
 
-引理 lem你好  : nat.
+论点 lem你好  : nat.
 证明.
   exact (myvar + myparam).
 据证实.
@@ -17,4 +49,7 @@ Check ( 对于所有 (n m : nat) (b : bool), n = if b then n else m ).
 
 Check (if true then 2 else 3).
 Compute (if true then 2 else 3).
-Compute ( 如果 true 然后 2 否则 3  ).
+
+Compute ( 若  true 则  2 否则 3  ).
+
+
