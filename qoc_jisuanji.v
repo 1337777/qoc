117af4582c6fb40bb3898395ddcc4905b953fa08
user1@localhost <user1@localhost>
2019-03-12 19:42:37 +0800

    init

--- qoc_jisuanji.v
+++ qoc_jisuanji.v
@@ -1,11 +1,121 @@
-From Qoc Require Import Jisuanji.
+From Qoc Require Import Jisuanji .
 
-归纳义 我的二进制  := 真 : 我的二进制 | 假 : 我的二进制 .
+Inductive infiniteNumbers : Type :=
+  Zero : infiniteNumbers
+| NextOne : infiniteNumbers -> infiniteNumbers .
+
+Section Section1 .
+  Variable pOutside : infiniteNumbers .
+  
+  Fixpoint addMore yInside : infiniteNumbers := 
+    match yInside with
+      Zero => pOutside
+    | NextOne xInside => NextOne (addMore xInside)
+    end .
+
+End Section1 .
+
+Lemma lemma1 : forall ( pOutsideInside : infiniteNumbers ) ,
+    addMore (NextOne Zero) pOutsideInside = addMore pOutsideInside (NextOne Zero) .
+Proof .
+  intros pOutsideInside.
+  simpl .
+  elim pOutsideInside .
+  - (** pOutsideInside is ( Zero ) *)
+    simpl .
+    reflexivity .
+  - (** pOutsideInside is ( NextOne qOutsideInside ) *)
+    intros qOutsideInside .
+    intros lemma1_qOutsideInside .
+    simpl .
+    rewrite lemma1_qOutsideInside .
+    reflexivity .
+Qed .
+
+Reset infiniteNumbers .
+
+归纳义 无限数字 : 类型 :=
+  零 : 无限数字
+| 下一个 : 无限数字 -> 无限数字 .
+
+部分 部分1 .
+  变量 p外 : 无限数字 .
+  
+  固定点 加更多 y内 : 无限数字 := 
+    匹配 y内 与
+      零 => p外
+    | 下一个 x内 => 下一个 (加更多 x内)
+    结束 .
+
+结束 部分1 .
+
+论点 论点1 : 对全部 ( p外内 : 无限数字 ) ,
+    加更多 (下一个 零) p外内 = 加更多 p外内 (下一个 零) .
+证明 .
+  介绍 p外内 .
+  简化 .
+  消除 p外内 .
+  - (** p外内 是 ( 零 ) *)
+    简化 .
+    同一 .
+  - (** p外内 是 ( 下一个 q外内 ) *)
+    介绍 q外内 .
+    介绍 论点1_q外内 .
+    简化 .
+    改写 论点1_q外内 .
+    同一 .
+据证实 .
+
+Reset 无限数字 .
+
+
+(**
+Guīnà yì wúxiàn shùzì: Lèixíng:=
+  Líng: Wúxiàn shùzì
+| xià yīgè: Wúxiàn shùzì -> wúxiàn shùzì.
+
+Bùfèn bùfèn 1.
+  Biànliàng p wài: Wúxiàn shùzì.
+  
+  Gùdìng diǎn jiā gèng duō y nèi: Wúxiàn shùzì:= 
+    Pǐpèi y nèi yǔ
+      líng => p wài
+    | xià yīgè x nèi => xià yīgè (jiā gèng duō x nèi)
+    jiéshù.
+
+Jiéshù bùfèn 1.
+
+Lùndiǎn lùndiǎn 1: Duì quánbù (p wài nèi: Wúxiàn shùzì),
+    jiā gèng duō (xià yīgè líng) p wài nèi = jiā gèng duō p wài nèi (xià yīgè líng).
+Zhèngmíng.
+  Jièshào p wài nèi.
+  Jiǎnhuà.
+  Xiāochú p wài nèi.
+  - (* P wài nèi shì (líng)*)
+    jiǎnhuà.
+    Tóngyī.
+  - (* P wài nèi shì (xià yīgè q wài nèi)*)
+    jièshào q wài nèi.
+    Jièshào lùndiǎn 1_q wài nèi.
+    Jiǎnhuà.
+    Gǎixiě lùndiǎn 1_q wài nèi.
+    Tóngyī.
+Jù zhèngshí.
+*)
+
+
+
+(** -------------------------------------------- **)
+
+
+
+归纳义 我的二进制  := 真 : 我的二进制
+                    | 假 : 我的二进制 .
 
 定义 abc或 ( x : 我的二进制 ) ( y : 我的二进制 ) :=
   ( 若 x 则 真 否则 y ) . 
 
-(* true or true = true. 1 + 1 = 1
+(** true or true = true. 1 + 1 = 1
 true or false = true. 1 + 0 = 1
 false or true  = true. 0 + 1 = 1
 false or false = false. 0 + 0 = 0 *)
@@ -17,18 +127,18 @@ false or false = false. 0 + 0 = 0 *)
   证明.
     展开 abc或.
     例子 x.
-    - (* x 真*)
+    - (** x 真*)
       例子 y.
-      + (* y 真 *) 同一.
-      + (* y 假 *) 同一.
-    - (* x 假 *)
+      + (** y 真 *) 同一.
+      + (** y 假 *) 同一.
+    - (** x 假 *)
       例子 y.
-      + (* y 真 *) 同一.
-      + (* y 假 *) 同一.
-    据证实.
+      + (** y 真 *) 同一.
+      + (** y 假 *) 同一.
+  据证实.
 结束 我的分1.
 
-(* Guīnà yì wǒ de èrjìnzhì  := Zhēn: Wǒ de èrjìnzhì | jiǎ: Wǒ de èrjìnzhì.
+(** Guīnà yì wǒ de èrjìnzhì  := Zhēn: Wǒ de èrjìnzhì | jiǎ: Wǒ de èrjìnzhì.
 
 Dìngyì huò (x : Wǒ de èrjìnzhì) (y : Wǒ de èrjìnzhì):= (Ruò x zé zhēn fǒuzé y). 
 
@@ -52,30 +162,6 @@ Jiéshù wǒ de fēn 1.
 
 
 
-
-
-
-
 (** -------------------------------------------- **)
-
-参数 myparam : nat.
-
-部分 section1.
-
-变量 myvar : nat.
-
-论点 lem你好  : nat.
-证明.
-  exact (myvar + myparam).
-据证实.
-
-结束 section1.
-
-Check ( 对于所有 (n m : nat) (b : bool), n = if b then n else m ). 
-
-Check (if true then 2 else 3).
-Compute (if true then 2 else 3).
-
-Compute ( 若  true 则  2 否则 3  ).
 
 
