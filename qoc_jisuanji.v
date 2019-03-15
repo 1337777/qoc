4979d5a74d1b0c836c26626fa9fa4970135ee376
user1@localhost <user1@localhost>
2019-03-15 18:55:51 +0800

    init

--- qoc_jisuanji.v
+++ qoc_jisuanji.v
@@ -7,6 +7,7 @@ Inductive infiniteNumbers : Type :=
 | NextOne : infiniteNumbers -> infiniteNumbers .
 
 Section Section1 .
+  
   Variable pOutside : infiniteNumbers .
   
   Fixpoint addMore yInside : infiniteNumbers := 
@@ -41,86 +42,76 @@ End infiniteNumbers .
 Print infiniteNumbers .
 
 包裹 无限数字包裹 .
-  
+(** Bāoguǒ wúxiàn shùzì bāoguǒ. *)  
+
 归纳义 无限数字 : 类型 :=
   零 : 无限数字
 | 下一个 : 无限数字 -> 无限数字 .
+(** Guīnà yì wúxiàn shùzì: Lèixíng:=
+  Líng: Wúxiàn shùzì
+| xià yīgè: Wúxiàn shùzì -> wúxiàn shùzì. *)
 
 部分 部分1 .
+(** Bùfèn bùfèn 1. *)
+
   变量 p外 : 无限数字 .
+  (** Biànliàng p wài: Wúxiàn shùzì. *)
   
   固定点 加更多 y内 : 无限数字 := 
     匹配 y内 与
       零 => p外
     | 下一个 x内 => 下一个 (加更多 x内)
     结束 .
-
-结束 部分1 .
-
-打印 加更多 .
-
-论点 论点1 : 对全部 ( p外内 : 无限数字 ) ,
-    加更多 (下一个 零) p外内 = 加更多 p外内 (下一个 零) .
-证明 .
-  介绍 p外内 .
-  简化 .
-  消除 p外内 .
-  - (** p外内 是 ( 零 ) *)
-    简化 .
-    同一 .
-  - (** p外内 是 ( 下一个 q外内 ) *)
-    介绍 q外内 .
-    介绍 论点1_q外内 .
-    简化 .
-    改写 论点1_q外内 .
-    同一 .
-据证实 .
-
-结束 无限数字包裹 .
-
-打印 无限数字包裹 .
-
-(**
-Bāoguǒ wúxiàn shùzì bāoguǒ.
-  
-Guīnà yì wúxiàn shùzì: Lèixíng:=
-  Líng: Wúxiàn shùzì
-| xià yīgè: Wúxiàn shùzì -> wúxiàn shùzì.
-
-Bùfèn bùfèn 1.
-  Biànliàng p wài: Wúxiàn shùzì.
-  
-  Gùdìng diǎn jiā gèng duō y nèi: Wúxiàn shùzì:= 
+  (** Gùdìng diǎn jiā gèng duō y nèi: Wúxiàn shùzì:= 
     Pǐpèi y nèi yǔ
       líng => p wài
     | xià yīgè x nèi => xià yīgè (jiā gèng duō x nèi)
-    jiéshù.
+    jiéshù. *)
 
-Jiéshù bùfèn 1.
+结束 部分1 .
+(** Jiéshù bùfèn 1. *)
 
-Dǎyìn jiā gèng duō.
+打印 加更多 .
+(** Dǎyìn jiā gèng duō. *)
 
-Lùndiǎn lùndiǎn 1: Duì quánbù (p wài nèi: Wúxiàn shùzì),
-    jiā gèng duō (xià yīgè líng) p wài nèi = jiā gèng duō p wài nèi (xià yīgè líng).
-Zhèngmíng.
-  Jièshào p wài nèi.
-  Jiǎnhuà.
-  Xiāochú p wài nèi.
-  - (** P wài nèi shì (líng)*)
-    jiǎnhuà.
-    Tóngyī.
-  - (** P wài nèi shì (xià yīgè q wài nèi)*)
-    jièshào q wài nèi.
-    Jièshào lùndiǎn 1_q wài nèi.
-    Jiǎnhuà.
-    Gǎixiě lùndiǎn 1_q wài nèi.
-    Tóngyī.
-Jù zhèngshí.
+论点 论点1 : 对全部 ( p外内 : 无限数字 ) ,
+    加更多 (下一个 零) p外内 = 加更多 p外内 (下一个 零) .
+(** Lùndiǎn lùndiǎn 1: Duì quánbù (p wài nèi: Wúxiàn shùzì),
+    jiā gèng duō (xià yīgè líng) p wài nèi = jiā gèng duō p wài nèi (xià yīgè líng). *)
+证明 .
+(** Zhèngmíng. *)
+  介绍 p外内 .
+  (** Jièshào p wài nèi. *)
+  简化 .
+  (** Jiǎnhuà. *)
+  消除 p外内 .
+  (** Xiāochú p wài nèi. *)  
+  - (** p外内 是 ( 零 ) *)
+    (** P wài nèi shì (líng) *)
+    简化 .
+    (** jiǎnhuà. *)
+    同一 .
+    (** Tóngyī. *)
+  - (** p外内 是 ( 下一个 q外内 ) *)
+    (** P wài nèi shì (xià yīgè q wài nèi) *)
+    介绍 q外内 .
+    (** jièshào q wài nèi. *)
+    介绍 论点1_q外内 .
+    (** Jièshào lùndiǎn 1_q wài nèi. *)
+    简化 .
+    (** Jiǎnhuà. *)
+    改写 论点1_q外内 .
+    (** Gǎixiě lùndiǎn 1_q wài nèi. *)
+    同一 .
+    (** Tóngyī. *)
+据证实 .
+(** Jù zhèngshí. *)
 
-Jiéshù wúxiàn shùzì bāoguǒ.
+结束 无限数字包裹 .
+(** Jiéshù wúxiàn shùzì bāoguǒ. *)
 
-Dǎyìn wúxiàn shùzì bāoguǒ.
-*)
+打印 无限数字包裹 .
+(** Dǎyìn wúxiàn shùzì bāoguǒ. *)
 
 
 
