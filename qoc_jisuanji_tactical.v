b1ed494c275860e5a2f70f303e51a53e7ce3481f
user1@localhost <user1@localhost>
2019-04-10 18:52:10 +0800

    init

--- qoc_jisuanji_tactical.v
+++ qoc_jisuanji_tactical.v
@@ -16,25 +16,22 @@ Outline ::
 
 Goal forall n : nat + nat , forall b : bool + bool , ( n = n /\ b = b ) .
   intros n b.
-  解构 n. Undo.
+  解构 n. destruct b.  Undo 2.
   解构 n  ; 解构 b . Undo .
 
   解构 n; ( 解构 b ;
-                [> idtac | idtac | idtac | idtac ] ) . Undo.
-  解构 n; ( inner: 解构 b ;
-               [> idtac | idtac ] ) . Undo.
-  解构 n; inner: 解构 b ;
-                       [> idtac | idtac ]  . Undo.
-  解构 n; ( 解构 b;
-                [ idtac | idtac ] ) . Undo.
-
-  (** nei *)
+                [> idtac & idtac & idtac & idtac ] ) . Undo.
   解构 n; ( 内: 解构 b ;
-               [> idtac | idtac ] ) . Undo.
+               [> idtac & idtac ] ) . Undo.
+  解构 n; 内: 解构 b ;
+                       [> idtac & idtac ]  . Undo.
+  解构 n; ( 解构 b;
+                [ idtac & idtac ] ) . Undo.
   
 Abort .
 
 Goal forall B C D : Prop, (False /\ B) /\ (C /\ D).
+  split. Undo.
   split; split. Undo.
   做 2 split. Undo.
   重复 split. Undo.
@@ -47,8 +44,41 @@ Goal forall B C D : Prop, (False /\ B) /\ (C /\ D).
 
 Abort.
 
+(*
+Goal forall n: Nat + nat, forall b: Bool + bool, (n = n/\ b = b).
+  Intros n b.
+  Jiěgòu n. Undo.
+  Jiěgòu n  ; jiěgòu b. Undo.
 
+  Jiěgòu n; (jiěgòu b;
+                [> idtac & idtac & idtac & idtac] ). Undo.
+  Jiěgòu n; (inner: Jiěgòu b;
+               [> idtac & idtac] ). Undo.
+  Jiěgòu n; inner: Jiěgòu b;
+                       [> idtac & idtac]  . Undo.
+  Jiěgòu n; (jiěgòu b;
+                [idtac & idtac] ). Undo.
 
+  (** Nei*)
+  jiěgòu n; (nèi: Jiěgòu b;
+               [> idtac & idtac] ). Undo.
+  
+Abort.
+
+Goal forall B C D: Prop, (False/\ B)/\ (C/\ D).
+  Split; split. Undo.
+  Zuò 2 split. Undo.
+  Chóngfù split. Undo.
+  Jìnzhǎn chóngfù split.
+  1: {
+    Shībài jìnzhǎn chóngfù split.
+    Chángshì jìnzhǎn chóngfù split.
+    Chéngrèn.
+  }
+
+Abort.
+
+*)
 
 (** --------------------------------------------------- *)
 
@@ -61,17 +91,17 @@ Goal forall n : nat + nat , forall b : bool + bool , ( n = n /\ b = b ) .
   destruct n  ; destruct b . Undo .
 
   destruct n; ( destruct b ;
-                [> idtac | idtac | idtac | idtac ] ) . Undo.
+                [> idtac & idtac & idtac & idtac ] ) . Undo.
   destruct n; ( inner: destruct b ;
-               [> idtac | idtac ] ) . Undo.
+               [> idtac & idtac ] ) . Undo.
   destruct n; inner: destruct b ;
-                       [> idtac | idtac ]  . Undo.
+                       [> idtac & idtac ]  . Undo.
   destruct n; ( destruct b;
-                [ idtac | idtac ] ) . Undo.
+                [ idtac & idtac ] ) . Undo.
 
   (** nei *)
   destruct n; ( 内: destruct b ;
-               [> idtac | idtac ] ) . Undo.
+               [> idtac & idtac ] ) . Undo.
   
 Abort .
 
